b5382b40769def7c0f41c4b570af0bd9e546be1b
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-02 17:14:45 +0800

    Added initial implementation to extractParameterListFromFunction procedure.

---
+++ extract-parameter-list-from-function.js
@@ -0,0 +1,33 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "extract-parameter-list-from-function",
+			"fileName": "extract-parameter-list-from-function.js",
+			"moduleName": "extractParameterListFromFunction",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/extract-parameter-list-from-function.git",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+
+	@include:
+	@end-include
+*/
+var extractParameterListFromFunction = function extractParameterListFromFunction( rawFunctionString ){
+	/*:
+		@meta-configuration:
+			{
+				"rawFunctionString:required": "string"
+			}
+		@end-meta-configuration
+	*/
+
+
+};
+
+( module || { } ).exports = extractParameterListFromFunction;
